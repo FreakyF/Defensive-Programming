c9402a0de750acad6fc2af06c275ac9f27b186d7
Kamil Fudala <kamil.fudala44@gmail.com>
2025-03-13 21:47:57 +0100

    Create Email Validator.

---
+++ src/main/java/pl/kielce/tu/fudala/lab01/banking/validator/email/EmailValidator.java
@@ -0,0 +1,21 @@
+﻿package pl.kielce.tu.fudala.lab01.banking.validator.email;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class EmailValidator implements ConstraintValidator<ValidEmail, String> {
+	private static final String EMAIL_PATTERN = "^s\\d{5}@student\\.tu\\.kielce\\.pl$";
+
+	@Override
+	public void initialize(ValidEmail constraintAnnotation) {
+		// Możesz tu zainicjować dodatkowe ustawienia, jeśli to konieczne
+	}
+
+	@Override
+	public boolean isValid(String email, ConstraintValidatorContext context) {
+		if (email == null) {
+			return false;
+		}
+		return email.matches(EMAIL_PATTERN);
+	}
+}
---
+++ src/main/java/pl/kielce/tu/fudala/lab01/banking/validator/email/ValidEmail.java
@@ -0,0 +1,23 @@
+﻿package pl.kielce.tu.fudala.lab01.banking.validator.email;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Constraint(validatedBy = EmailValidator.class)
+@Target({FIELD, METHOD, PARAMETER, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+public @interface ValidEmail {
+	String message() default "Invalid email format";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+}
