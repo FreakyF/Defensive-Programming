14f0b45e18150286628f051af25d448fa9806f37
Kamil Fudala <kamil.fudala44@gmail.com>
2025-03-13 23:00:31 +0100

    Implement FileStorage.

---
+++ src/main/java/pl/kielce/tu/fudala/lab01/banking/persistence/FileStorage.java
@@ -0,0 +1,30 @@
+﻿package pl.kielce.tu.fudala.lab01.banking.persistence;
+
+import pl.kielce.tu.fudala.lab01.banking.serialization.IJsonMapper;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileStorage implements IFileStorage {
+
+    private final IJsonMapper jsonMapper;
+
+    public FileStorage(IJsonMapper jsonMapper) {
+        this.jsonMapper = jsonMapper;
+    }
+
+    @Override
+    public <T> void save(String filePath, T data) throws IOException {
+        String json = jsonMapper.toJson(data);
+        Files.writeString(Paths.get(filePath), json);
+    }
+
+    @Override
+    public <T> T load(String filePath, Class<T> clazz) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+        String json = new String(bytes, StandardCharsets.UTF_8);
+        return jsonMapper.fromJson(json, clazz);
+    }
+}
---
+++ src/main/java/pl/kielce/tu/fudala/lab01/banking/persistence/IFileStorage.java
@@ -0,0 +1,9 @@
+﻿package pl.kielce.tu.fudala.lab01.banking.persistence;
+
+import java.io.IOException;
+
+public interface IFileStorage {
+    <T> void save(String filePath, T data) throws IOException;
+
+    <T> T load(String filePath, Class<T> clazz) throws IOException;
+}
