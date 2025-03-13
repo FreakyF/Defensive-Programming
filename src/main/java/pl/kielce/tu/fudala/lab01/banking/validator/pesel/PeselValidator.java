3ff2ea51b918bfe829d79f513b35a5a659eee4d6
Kamil Fudala <kamil.fudala44@gmail.com>
2025-03-13 21:59:01 +0100

    Create PESEL Validator.

---
+++ src/main/java/pl/kielce/tu/fudala/lab01/banking/validator/pesel/PeselValidator.java
@@ -0,0 +1,29 @@
+﻿package pl.kielce.tu.fudala.lab01.banking.validator.pesel;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PeselValidator implements ConstraintValidator<ValidPesel, String> {
+	@Override
+	public boolean isValid(String pesel, ConstraintValidatorContext context) {
+		if (pesel == null) {
+			return false;
+		}
+
+		if (!pesel.matches("\\d{11}")) {
+			return false;
+		}
+
+		int[] weights = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
+		int sum = 0;
+		for (int i = 0; i < weights.length; i++) {
+			int digit = Character.getNumericValue(pesel.charAt(i));
+			sum += digit * weights[i];
+		}
+		int modulo = sum % 10;
+		int checkDigit = modulo == 0 ? 0 : (10 - modulo);
+
+		int lastDigit = Character.getNumericValue(pesel.charAt(10));
+		return checkDigit == lastDigit;
+	}
+}
---
+++ src/main/java/pl/kielce/tu/fudala/lab01/banking/validator/pesel/ValidPesel.java
@@ -0,0 +1,23 @@
+﻿package pl.kielce.tu.fudala.lab01.banking.validator.pesel;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Constraint(validatedBy = PeselValidator.class)
+@Target({FIELD, METHOD, PARAMETER, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+public @interface ValidPesel {
+	String message() default "Invalid PESEL number";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+}
